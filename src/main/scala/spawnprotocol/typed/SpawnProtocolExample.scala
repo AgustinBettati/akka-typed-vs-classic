2be606704e2b1862dd3a52c052fc99866c300cbe
Agustin Bettati <bettatiagustin@gmail.com>
2022-03-30 10:34:44 -0300

    example of spawn protocol

---
+++ src/main/scala/spawnprotocol/typed/SpawnProtocolExample.scala
@@ -0,0 +1,37 @@
+package spawnprotocol.typed
+import akka.actor.typed.{ActorRef, ActorSystem, Behavior, Props, SpawnProtocol}
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.scaladsl.LoggerOps
+import akka.util.Timeout
+import stateful.typed.TypedStatefulActor
+
+import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.duration.DurationInt
+
+object ActorWithSpawnProtocol {
+  def apply(): Behavior[SpawnProtocol.Command] =
+    Behaviors.setup { context =>
+      // Start initial tasks
+      // context.spawn(...)
+      SpawnProtocol()
+    }
+}
+
+object SpawnProtocolExample extends App {
+
+  implicit val system: ActorSystem[SpawnProtocol.Command] =
+    ActorSystem(ActorWithSpawnProtocol(), "hello")
+
+  import akka.actor.typed.scaladsl.AskPattern._
+  implicit val ec: ExecutionContext = system.executionContext
+  implicit val timeout: Timeout = Timeout(3.seconds)
+
+
+  val spawnedCounter: Future[ActorRef[TypedStatefulActor.Command]] =
+    system.ask(SpawnProtocol.Spawn(TypedStatefulActor(), name = "", props = Props.empty, _))
+
+  for (counter <- spawnedCounter) {
+    counter ! TypedStatefulActor.Add
+  }
+  system.terminate()
+}
